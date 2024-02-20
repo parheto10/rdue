9bf3a7ff4371f2b64aad4f76fadd2be36c580ecc
Ambeu <69042263+Ambeu@users.noreply.github.com>
2024-02-20 14:52:46 +0000

    création d'un fichier log

--- backend/settings/settings.py
+++ backend/settings/settings.py
@@ -28,6 +28,25 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
+LOGGING = {
+    'version':1,
+    'disable_existing_loggers':False,
+    'handlers':{
+        'file':{
+            'level':'INFO',
+            'class':'logging.FileHandler',
+            'filename':'debug.log',
+        }
+    },
+    'loggers':{
+            'django':{
+                'handlers':['file'],
+                'level':'INFO',
+                'propagate':True
+            }
+        }
+}
+
 
 # Application definition
 
