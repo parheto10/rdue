0c46484cb613a9ad617ce45ffb533dd00b3a650d
Parheto <parheto10@gmail.com>
2024-08-26 16:41:31 +0000

    New Update with front Translate

--- backend/settings/settings.py
+++ backend/settings/settings.py
@@ -87,6 +87,17 @@ DATABASES = {
     }
 }
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'akidompro',
+#         'HOST': 'localhost',
+#         'PORT': 3306,
+#         'USER': 'root',
+#         'PASSWORD': 'P@premium2@#',
+#     }
+# }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
