d0aa87e5b970583e3bce5414dff92a84214d129d
Ambeu <69042263+Ambeu@users.noreply.github.com>
2024-10-14 11:48:51 +0000

    backend pour les coopérative de Agromap

--- backend/settings/settings.py
+++ backend/settings/settings.py
@@ -102,8 +102,12 @@ WSGI_APPLICATION = 'settings.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'cooperative',
+        'HOST':'localhost',
+        'PORT':'3306',
+        'USER':'root', 
+        'PASSWORD':'root'  
     }
 }
 
