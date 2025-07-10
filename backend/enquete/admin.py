40f8ca9f5788da322ffbd9e975f6bc6a7336d0c7
AngeDago <dagodassiange@gmail.com>
2025-07-10 10:08:00 +0000

    Refactor admin registration to use ImportExportModelAdmin for enhanced functionality

--- backend/enquete/admin.py
+++ backend/enquete/admin.py
@@ -2,9 +2,21 @@ from django.contrib import admin
 
 from .models import TypeEnquete, Enquete, TypeQuestion, Question, Reponse, Enqueteur
 
-admin.site.register(TypeEnquete)
-admin.site.register(Enquete)
-admin.site.register(TypeQuestion)
-admin.site.register(Question)
-admin.site.register(Reponse)
-admin.site.register(Enqueteur)
+from import_export.admin import ImportExportModelAdmin
+
+class QuestionAdmin(ImportExportModelAdmin):
+    list_display = ['id', 'libelle', 'est_obligatoire', 'type_question', 'enquete']
+    list_filter = ['enquete__identifiant', 'type_question__libelle']
+    search_fields = ['libelle', 'enquete__identifiant']
+
+class ReponseAdmin(ImportExportModelAdmin):
+    list_display = ['id', 'enquete', 'repondant', 'submitted_at']
+    list_filter = ['enquete__identifiant', 'repondant__nom']
+    search_fields = ['enquete__identifiant', 'repondant__nom']
+
+admin.site.register(TypeEnquete, ImportExportModelAdmin)
+admin.site.register(Enquete, ImportExportModelAdmin)
+admin.site.register(TypeQuestion, ImportExportModelAdmin)
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Reponse, ReponseAdmin)
+admin.site.register(Enqueteur, ImportExportModelAdmin)
